4d2a64e2b654e1d45c022cd266855ee29704e633
Bruno Gaspar <bgrbarbosa@hotmail.com>
2023-03-28 10:28:38 -0300

    Projeto inicial

---
+++ src/test/java/br/com/bgsis/cadastro_eventos/CadastroEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bgsis.cadastro_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
